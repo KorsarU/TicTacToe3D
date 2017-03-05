ad5c695784918f6b7f72a9c5a4fa72619df390a0
Фёдор Березюк < >
2017-03-05 13:35:28 +0300

    repair TTTT2 evaluation for more random moves, add methods, for analysis time per round

--- TTTT2.m
+++ TTTT2.m
@@ -17,7 +17,8 @@ else if s==1
         return;
     end
 end
-
+counter_max = 1;
+counter_min = 1;
 max_x = [-10000,0,0,0];
 min_x = [10000,0,0,0];
 for d=1:n
@@ -30,18 +31,31 @@ for d=1:n
                 x = sum(sum(sum(tmptest)));
                 if x>max_x(1)
                     max_x = [x,i,j,d];
+                    counter_max = 1;
+                else
+                    if x==max_x(1)
+                        counter_max = counter_max+1;
+                        max_x(counter_max,:) = [x,i,j,d];
+                    end
                 end
                 if x<min_x(1)
                     min_x = [x,i,j,d];
+                    counter_min = 1;
+                else
+                    if x==min_x(1)
+                        counter_min = counter_min + 1;
+                        min_x(counter_min,:) = [x,i,j,d];
+                    end
                 end
+                
             end
         end
     end
 end
 
 if player==1
-    res = max_x(2:4);
+    res = max_x(randi(counter_max,1),2:4);
 else
-    res = min_x(2:4);
+    res = min_x(randi(counter_min,1),2:4);
 end
 end
--- game.m
+++ game.m
@@ -1,80 +1,85 @@
 %Enter point
-function game()
+function time = game()
 
-player1 = 'human';
+count_of_rounds = 10;
+sum_of_rounds = zeros(1,count_of_rounds);
+
+for games = 1:count_of_rounds
+player1 = 'ai   ';
 player2 = 'ai   ';
 isTerminated = false;
 currentPlayer = 1;
 
 %deph of the searching
-deph = 2;
+deph = 3;
 
 %select size of the board
 boardSize = 4;
 board = zeros(boardSize,boardSize,boardSize);
-
    
-sc = 0;     
+sc = 0;
 
-while ~isTerminated
     time = clock;
-    if currentPlayer == 1
-        disp('player1 moves');
-        if player1 == 'human'
-            prompt = 'Your turn:';
-            ok = false;
-            while ~ok
-                x = input(prompt);
-                if board(x)==0
-                    ok =true;
-                else
-                    warning('Wrong move! Please try again');
+    while ~isTerminated
+
+        if currentPlayer == 1
+            disp('player1 moves');
+            if player1 == 'human'
+                prompt = 'Your turn:';
+                ok = false;
+                while ~ok
+                    x = input(prompt);
+                    if board(x)==0
+                        ok =true;
+                    else
+                        warning('Wrong move! Please try again');
+                    end
                 end
-            end
-            if size(x,2)>1
-                board(x(1),x(2),x(3)) = currentPlayer;
+                if size(x,2)>1
+                    board(x(1),x(2),x(3)) = currentPlayer;
+                else
+                    board(x) = currentPlayer;
+                end
+                currentPlayer = -currentPlayer;
             else
+                q = playTTTT(board,deph,currentPlayer,3);
+                if(size(q)==1)
+                    board(q) = currentPlayer;
+                else
+                    board(q(1),q(2),q(3)) = currentPlayer;
+                end
+                currentPlayer = -currentPlayer;
+            end
+        else
+            disp('player2 moves');
+            if player2 == 'human'
+                prompt = 'Your turn:';
+                ok = false;
+                while ~ok
+                    x = input(prompt);
+                    if board(x)==0
+                        ok =true;
+                    else
+                        warning('Wrong move! Please try again');
+                    end
+                end
                 board(x) = currentPlayer;
-            end
-            currentPlayer = -currentPlayer;
-        else
-            q = playTTTT(board,deph,currentPlayer,3);
-            board(q(1)) = currentPlayer;
-            currentPlayer = -currentPlayer;
-        end
-    else
-        disp('player2 moves');
-        if player2 == 'human'
-            prompt = 'Your turn:';
-            ok = false;
-            while ~ok
-                x = input(prompt);
-                if board(x)==0
-                    ok =true;
-                else
-                    warning('Wrong move! Please try again');
-                end
-            end
-            board(x) = currentPlayer;
-            currentPlayer = -currentPlayer;
-        else
-            q = playTTTT(board,deph,currentPlayer,1);
-            if(size(q)==1)
-                board(q) = currentPlayer;
+                currentPlayer = -currentPlayer;
             else
-                board(q(1),q(2),q(3)) = currentPlayer;
+                q = playTTTT(board,deph,currentPlayer,1);
+                if(size(q)==1)
+                    board(q) = currentPlayer;
+                else
+                    board(q(1),q(2),q(3)) = currentPlayer;
+                end
+                currentPlayer = -currentPlayer;
             end
-            currentPlayer = -currentPlayer;
         end
+        
+        [isTerminated, sc] = checkBoard(board);
     end
     showBoard(board);
-    if(currentPlayer == 1)
-        etime(clock, time)
-    end
-            
-    [isTerminated, sc] = checkBoard(board);
-end
-
+    sum_of_rounds(games) = etime(clock, time);
     if sc == 0
             disp('It is a draw');
     elseif sc > 0
@@ -82,6 +87,10 @@ end
     elseif sc < 0
             disp('O wins');
     end
+end
+    time = sum(sum_of_rounds)/count_of_rounds;
+
+    
 
 end
 
